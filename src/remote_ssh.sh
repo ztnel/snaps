c0de171c5fe614d4a618766498c245d5352c1342
Christian <christian911@sympatico.ca>
2020-04-11 05:43:08 -0400

    sh fixes

--- src/remote_ssh.sh
+++ src/remote_ssh.sh
@@ -6,6 +6,7 @@ chmod 0700 $HOME/.ssh
 # generate default RSA key pair
 ssh-keygen
 
+# Server connection details
 echo Server name:
 read user_name
 echo Server IP
@@ -14,8 +15,8 @@ read server_ip
 # Server SSH Key install
 # Simply install the public key on the server device from the client machine.
 
-ssh-copy-id user_name@server_ip
+ssh-copy-id $user_name@$server_ip
 
 # Run Remote Connection
 # Login to Ubuntu 18.04 LTS server with `ssh` from client using ssh keys
-ssh user_name@server_ip
+ssh $user_name@$server_ip
